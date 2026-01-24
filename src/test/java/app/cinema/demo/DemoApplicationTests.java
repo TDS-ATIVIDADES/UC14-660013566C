2cdc905b8191c67a423d0f4526935ac4a8ff5f9e
Wagner Sousa <wagner.fernandes.sousa@outlook.com>
2026-01-24 15:12:32 +0000

    🎉 Implementação inicial do projeto base.

---
+++ src/test/java/app/cinema/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package app.cinema.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
